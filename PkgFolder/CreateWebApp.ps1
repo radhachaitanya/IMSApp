b491d175c96055ced8a84479ae59adbbf88f5f38
radhachaitanya <v-rasrim@microsoft.com>
2018-09-28 21:17:58 +0530

    IMS AppSource Deployment

---
+++ PkgFolder/CreateWebApp.ps1
@@ -0,0 +1,186 @@
+﻿# parameters of Azure ResourceGroupName, Subscriptionid ,Forms , Mappings and StorageKey Path's passing from Dll 
+param 
+(   
+ [string]$resourceGroupName,
+ [string]$subscriptionId,
+ [string]$formsPath,
+ [string]$mappingsPath,
+ [string]$storageKeyPath,
+ [string]$orgName
+)
+##########################################
+$location = 'Central US'  #By default Azure Location is setting to 'Central Us'
+
+$IMSAppWebJobs   = 'IMSAppWebJobs'+$orgName
+$IMSAppPrintAll  = 'IMSAppPrintAll'+$orgName
+$IMSAppBinder    = 'IMSAppBinder'+$orgName
+$IMSAppForms     = 'IMSAppForms'+$orgName
+$IMSAppServicePlan = 'IMSAppServicePlan'
+
+
+#Login to Azure account
+Login-AzureRmAccount
+# select Subscription
+Set-AzureRmContext  -SubscriptionId $subscriptionId 
+
+# Create new resource group if not exists.
+$rgAvail = Get-AzureRmResourceGroup -Name $resourceGroupName -Location $location -ErrorAction SilentlyContinue
+if(!$rgAvail){
+    $resourceGroupName ='IMSAppResourceGroup'
+    $resourceGroupName = New-AzureRmResourceGroup -Name $resourceGroupName -Location $location	
+	$resourceGroupName = $resourceGroupName.ResourceGroupName
+}
+
+$IMSAppAllAppServicePlan = Get-AzureRmAppServicePlan -ResourceGroupName $resourceGroupName -Name $IMSAppServicePlan
+if(!$IMSAppAllAppServicePlan)
+{
+New-AzureRmAppServicePlan -Name $IMSAppServicePlan -Location $location -ResourceGroupName $resourceGroupName -Tier Free
+}
+$IMSAppAllAppServicePlan = Get-AzureRmAppServicePlan -ResourceGroupName $resourceGroupName -Name $IMSAppServicePlan
+
+
+
+
+#Create new WebApp for WebJobs Application if not exists
+$webAppWebJobs = Get-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $IMSAppWebJobs -ErrorAction SilentlyContinue
+
+if(!$webAppWebJobs -And $IMSAppAllAppServicePlan)
+{
+New-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $IMSAppWebJobs -AppServicePlan $IMSAppServicePlan -Location $location
+}
+
+#Create new WebApp for PrintAll Application if not exists
+$webAppPrintAll = Get-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $IMSAppPrintAll -ErrorAction SilentlyContinue
+if(!$webAppPrintAll -And $IMSAppAllAppServicePlan)
+{
+New-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $IMSAppPrintAll -AppServicePlan $IMSAppServicePlan -Location $location
+}
+
+#Create new WebApp for Binder Application if not exists
+$webAppBinder = Get-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $IMSAppBinder -ErrorAction SilentlyContinue
+if(!$webAppBinder -And $IMSAppAllAppServicePlan)
+{
+New-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $IMSAppBinder -AppServicePlan $IMSAppServicePlan -Location $location
+}
+
+#Create new WebApp for FormsManagment Application if not exists
+$webAppFormsManagement = Get-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $IMSAppForms -ErrorAction SilentlyContinue
+if(!$webAppFormsManagement -And $IMSAppAllAppServicePlan)
+{
+New-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $IMSAppForms -AppServicePlan $IMSAppServicePlan -Location $location
+}
+	
+   $StorageAccountName = 'imsappformstrge'+$orgName # between 3 to 24 charactors and should be lower-caseLetters only
+                                                 # Create the storage account if it doesn't already exist
+   $checkStorageAccountExists = Get-AzureRmStorageAccount -Name $StorageAccountName -ResourceGroupName $resourceGroupName -ErrorAction Ignore
+    if($checkStorageAccountExists  -eq $null){    
+      $StorageAccount = New-AzureRmStorageAccount -AccountName $StorageAccountName -ResourceGroupName $resourceGroupName -Location $location -SkuName "Standard_LRS"
+	 
+    }
+	$IMSAppStorageAppInsight = 'IMSAppStorageAppInsight'+$orgName
+	#creating AppInsight for FormsManagement Application if not exists
+	$MyRes=Get-AzureRmResource -ResourceName $IMSAppStorageAppInsight -ResourceGroupName $resourceGroupName
+if ($null -eq $MyRes) {
+  	 $resource =  New-AzureRmResource -ResourceName $IMSAppStorageAppInsight -ResourceGroupName $resourceGroupName -Tag @{ applicationType = "web"; applicationName = $IMSAppForms} -ResourceType "Microsoft.Insights/components" -Location "East US" -PropertyObject @{"Application_Type"="web"} -Force  
+	 $AppInsightName =  $resource.Name
+     $IKey = $resource.Properties.InstrumentationKey
+}
+
+# If the StorageAccoount exists then create Containers -- 'forms' and 'mappings'
+$storageAccount = Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -AccountName $StorageAccountName
+ if($storageAccount)
+{
+$storageAccountKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -Name $StorageAccountName).Value[0]
+$ctx = $storageAccount.Context
+$StorageAccountContext = (Get-AzureRmStorageAccount | Where-Object{$_.StorageAccountName -eq $StorageAccountName}).Context
+
+	    $ContainerName = 'forms'
+	    $storageContainer = Get-AzureStorageContainer -Context $ctx -ErrorAction Stop | where-object {$_.Name -eq $ContainerName}	
+        If($storageContainer)
+        {        
+		   $formsInAzureCount = 0
+		   $listOfBLobs = Get-AzureStorageBlob -Container $ContainerName -Context $ctx 
+		   $listOfBlobs | ForEach-Object { $formsInAzureCount++ }
+		   if(@(Get-ChildItem $formsPath).Count -ne $formsInAzureCount)
+							{	
+							 Get-AzureStorageBlob -Context $ctx -Container $ContainerName | Remove-AzureStorageBlob						        	                       							
+									   $files = Get-ChildItem $formsPath
+									   foreach($file in $files)
+									   {
+									   $localfile = $formsPath+"\"+$file
+									   $remotefile = "$file"
+									   Set-AzureStorageBlobContent -File $localfile -Container $ContainerName -Blob $remotefile -Context $ctx -Force:$Force 
+									   }
+			                }					
+        }
+        Else 
+        {
+            New-AzureStorageContainer -Name  $ContainerName -Context $StorageAccountContext -Permission blob  -ErrorAction SilentlyContinue *>&1
+               $files = Get-ChildItem $formsPath
+			   foreach($file in $files)
+			   {
+			   $localfile = $formsPath+"\"+$file
+			   $remotefile = "$file"
+			   Set-AzureStorageBlobContent -File $localfile  -Container $ContainerName -Blob $remotefile -Context $ctx -Force:$Force 
+			   }
+	    }
+
+		 $ContainerName = 'mappings'
+		 $storageContainer = Get-AzureStorageContainer -Context $ctx -ErrorAction Stop | where-object {$_.Name -eq $ContainerName}	
+        If($storageContainer)
+        {         
+		   $mappingsInAzureCount = 0
+		   $listOfBLobs = Get-AzureStorageBlob -Container $ContainerName -Context $ctx 
+		   $listOfBlobs | ForEach-Object { $mappingsInAzureCount++ }
+		   if(@(Get-ChildItem $mappingsPath).Count -ne $mappingsInAzureCount)
+							{							        
+	                         Get-AzureStorageBlob -Context $ctx -Container $ContainerName | Remove-AzureStorageBlob		
+									   $files = Get-ChildItem $mappingsPath
+									   foreach($file in $files)
+									   {
+									   $localfile = $mappingsPath+"\"+$file
+									   $remotefile = "$file"
+									   Set-AzureStorageBlobContent -File $localfile -Container $ContainerName -Blob $remotefile -Context $ctx -Force:$Force 
+									   }
+			                }					
+        }
+        Else 
+        {
+            New-AzureStorageContainer -Name  $ContainerName -Context $StorageAccountContext -Permission blob  -ErrorAction SilentlyContinue *>&1
+               $files = Get-ChildItem $mappingsPath
+			   foreach($file in $files)
+			   {
+			   $localfile = $mappingsPath+"\"+$file
+			   $remotefile = "$file"
+			   Set-AzureStorageBlobContent -File $localfile  -Container $ContainerName -Blob $remotefile -Context $ctx -Force:$Force 
+			   }
+	    }
+
+  $path = $storageKeyPath +'StorageKey.txt'
+  if (Test-Path $path) 
+     {
+       Clear-Content $path 
+	   Add-Content -Path $path -Value $storageAccountKey
+     } 
+	 else	 
+	 {
+      New-Item -Path $storageKeyPath -Name "StorageKey.txt" -ItemType "file" -Value $storageAccountKey
+     }
+
+  } 
+  # Creating storage Account , KeyVault for IMSAppPrintAll and Binder
+
+    $StorageAccountName = 'imsappstorageacc'+$orgName # between 3 to 24 charactors and should be lower-caseLetters only
+                                                 # Create the storage account if it doesn't already exist
+   $checkStorageAccountExists = Get-AzureRmStorageAccount -Name $StorageAccountName -ResourceGroupName $resourceGroupName -ErrorAction Ignore
+    if($checkStorageAccountExists  -eq $null){
+      $ResourceGroupName = $resourceGroupName     
+      $StorageAccount = New-AzureRmStorageAccount -AccountName $StorageAccountName -ResourceGroupName $resourceGroupName -Location $location -SkuName "Standard_LRS"    	 
+    }
+
+	        $IMSAppKeyVaults='ImsAppKeyVaults'+$orgName
+			$KeyVault = Get-AzureRMKeyVault -VaultName $IMSAppKeyVaults
+			if(!$KeyVault)
+			{
+			New-AzureRmKeyVault -VaultName $IMSAppKeyVaults -ResourceGroupName $resourceGroupName -Location $location
+			}
---
+++ PkgFolder/PublishWebApp.ps1
@@ -0,0 +1,120 @@
+﻿#Passing the AzureWebJobs and AzureWebSites bin folder paths as parameters 
+param 
+(   
+ [string]$ExpireTodayValidToDocuments_Path,
+ [string]$SendDocumentExpirationNotificationToFN_Path ,
+ [string]$SLARemainingTime_Path ,
+ [string]$UpdateSlaFlagonActiveCases_Path ,
+ [string]$CreateTaskOnDepenedetnAgeChange_Path ,  
+ [string]$UpdateAttorneyInFNandCases_Path ,
+ [string]$IMSAppSourcePrintAll_Path,
+ [string]$IMSAppSourceBinder_Path,
+ [string]$IMSAppSourceFormsManagement_Path,
+ [string]$resourceGroupName,
+ [string]$subscriptionId,
+ [string]$orgName
+
+)
+#Enter Location
+
+$location = 'Central US'
+##########################################
+#$PublishedPath = $ToolPath"\\PkgFolder\PublishedStatus"
+$Apiversion = "2016-08-01"
+$webAppName = 'IMSAppWebJobs'+$orgName
+$IMSAppPrintAll  = 'IMSAppPrintAll'+$orgName
+$IMSAppBinder    = 'IMSAppBinder'+$orgName
+$IMSAppForms     = 'IMSAppForms'+$orgName
+
+$ExpireTodayValidToDocuments_UrI = 'https://'+$webAppName+'.scm.azurewebsites.net/api/triggeredwebjobs/ExpireTodayValidToDocuments'
+$SendDocumentExpirationNotificationToFN_UrI = 'https://'+$webAppName+'.scm.azurewebsites.net/api/triggeredwebjobs/SendDocumentExpirationNotificationToFN'
+$SLARemainingTime_UrI = 'https://'+$webAppName+'.scm.azurewebsites.net/api/triggeredwebjobs/SLARemainingTime'
+$UpdateSlaFlagonActiveCases_UrI ='https://'+$webAppName+'.scm.azurewebsites.net/api/triggeredwebjobs/UpdateSlaFlagonActiveCases'
+$CreateTaskOnDependentAgeChange_UrI = 'https://'+$webAppName+'.scm.azurewebsites.net/api/triggeredwebjobs/CreateTaskOnDependentAgeChange'
+$UpdateAttorneyInFNandCases_UrI = 'https://'+$webAppName+'.scm.azurewebsites.net/api/triggeredwebjobs/UpdateAttorneyInFNandCases'
+
+Add-AzureAccount 
+Select-AzureSubscription -SubscriptionId $subscriptionId  
+
+#New-Item  $ToolPath\PkgFolder\PublishedStatus -type file
+#Add-content $ToolPath\PkgFolder\PublishedStatus -value "Applications Deployment Start $(Get-Date)"
+
+#Login to Azure account
+#Connect-AzureRmAccount 
+#select Subscription
+#Set-AzureRmContext  -SubscriptionId $subscriptionId 
+
+# if ResourceGroupName not mentioned in the script , select default ResourceGroupName as "IMSAppResourceGroup"
+if($resourceGroupName -eq '')
+{
+  $resourceGroupName = 'IMSAppResourceGroup'
+}
+
+ #Publishing the WebSite of IMSAppPrintAll
+ $webAppPrintAll = Get-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $IMSAppPrintAll -ErrorAction SilentlyContinue
+if($webAppPrintAll)
+{
+ Publish-AzureWebsiteProject -name $IMSAppPrintAll -package $IMSAppSourcePrintAll_Path
+ #Add-content $PublishedPath -value "Successfully Deployed the PrintAll Application"
+}
+  #Publishing the WebSite of IMSAppBinder
+  $webAppBinder = Get-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $IMSAppBinder -ErrorAction SilentlyContinue
+if($webAppBinder)
+{
+ Publish-AzureWebsiteProject -name $IMSAppBinder -package $IMSAppSourceBinder_Path
+ #Add-content $PublishedPath -value "Successfully Deployed the Binder Application"
+}
+  #Publishing the WebSite of IMSAppForms
+  $webAppForms = Get-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $IMSAppForms -ErrorAction SilentlyContinue
+if($webAppForms)
+{
+ Publish-AzureWebsiteProject -name $IMSAppForms -package $IMSAppSourceFormsManagement_Path
+  #Add-content $PublishedPath -value "Successfully Deployed the Forms Application"
+}
+ 
+
+#Getting the publishingCredentials
+ function Get-PublishingProfileCredentials($resourceGroupName, $webAppName){
+ $resourceType = "Microsoft.Web/sites/config"
+ $resourceName = "$webAppName/publishingcredentials"
+ $publishingCredentials = Invoke-AzureRmResourceAction -ResourceGroupName $resourceGroupName -ResourceType $resourceType -ResourceName $resourceName -Action list -ApiVersion $Apiversion -Force
+    return $publishingCredentials
+ }
+#Getting the Azure Claims 
+ function Get-KuduApiAuthorisationHeaderValue($resourceGroupName, $webAppName){
+ $publishingCredentials = Get-PublishingProfileCredentials $resourceGroupName $webAppName
+ return ("Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $publishingCredentials.Properties.PublishingUserName, $publishingCredentials.Properties.PublishingPassword))))
+}
+ #Generating Token to Authicate the Azure.
+ $accessToken = Get-KuduApiAuthorisationHeaderValue $resourceGroupName $webAppname
+ #Generating header to create and publish the Webjob :
+ $Header = @{
+    'Content-Disposition'='attachment; filename=ConsoleApp.zip' 
+	'Authorization'=$accessToken
+	}
+
+ $webAppWebjobs = Get-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $webAppName -ErrorAction SilentlyContinue
+if($webAppWebjobs)
+{
+ # Creating the WebJob of ExpireTodayValidToDocuments
+ $result = Invoke-RestMethod -Uri $ExpireTodayValidToDocuments_UrI  -Headers $Header -Method put -InFile $ExpireTodayValidToDocuments_Path -ContentType 'application/zip'
+
+ # Creating the WebJob of SendDocumentExpirationNotificationToFN
+ $result = Invoke-RestMethod -Uri $SendDocumentExpirationNotificationToFN_UrI -Headers $Header -Method put -InFile $SendDocumentExpirationNotificationToFN_Path -ContentType 'application/zip'
+
+ # Creating the WebJob of SLARemainingTime
+ $result = Invoke-RestMethod -Uri $SLARemainingTime_UrI -Headers $Header -Method put -InFile $SLARemainingTime_Path -ContentType 'application/zip'
+  
+ # Creating the WebJob of UpdateSlaFlagonActiveCases
+ $result = Invoke-RestMethod -Uri $UpdateSlaFlagonActiveCases_UrI  -Headers $Header -Method put -InFile $UpdateSlaFlagonActiveCases_Path -ContentType 'application/zip'
+ 
+ # Creating the WebJob of CreateTaskOnDependentAgeChange
+ $result = Invoke-RestMethod -Uri $CreateTaskOnDependentAgeChange_UrI -Headers $Header -Method put -InFile $CreateTaskOnDepenedetnAgeChange_Path -ContentType 'application/zip'
+
+  # Creating the WebJob of UpdateAttorneyInFNandCases
+   $result = Invoke-RestMethod -Uri $UpdateAttorneyInFNandCases_UrI -Headers $Header -Method put -InFile $UpdateAttorneyInFNandCases_Path -ContentType 'application/zip'
+ 
+  #Add-content $PublishedPath -value "Successfully Deployed the Webjobs Application"
+}
+
+#Add-content $PublishedPath -value "Applications Deployment End $(Get-Date)"
